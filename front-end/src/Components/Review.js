a219c8c127569f4aa8d1976a8902cf9c2e5f9a97
rae charles <r_charles87@yahoo.com>
2022-02-18 13:40:36 -0500

    filled out review files

---
+++ front-end/src/Components/Review.js
@@ -0,0 +1,31 @@
+import ReviewForm from "./ReviewForm";
+import { useState } from "react";
+
+
+function Review({review, handleDelete, handleSubmit}) {
+    const [viewEditForm, setViewEditForm] = useState(false)
+
+    const toggleView = () => {
+        setViewEditForm(!viewEditForm);
+    };
+
+  return (
+    <div>
+        <button>Edit This Review</button>
+        {viewEditForm ? (
+            <ReviewForm reviewDetails={review} handleSubmit={handleSubmit} toggleView={toggleView} />
+        ) : (
+            <div>
+                <h4>
+                    {review.title} <span>{review.rating}</span>
+                </h4>
+                <h5>{review.reviewer}</h5>
+                <p>{review.content}</p>
+            </div>
+        )}
+        <button onClick={()=>handleDelete(review.id)}>Delete</button>
+    </div>
+  )
+}
+
+export default Review
